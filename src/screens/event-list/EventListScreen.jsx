01884b410c4d2489c3957180186de681b4f4c6e5
Ezequiel Cruz <prgrez210@gmail.com>
2023-04-17 17:12:03 -0300

    Agregado de tab-navigation

---
+++ src/screens/event-list/EventListScreen.jsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export const EventListScreen = () => {
+    return(
+        <View>
+            <Text>List of events</Text>
+        </View>
+    );
+};
---
+++ src/screens/profile/ProfileScreen.jsx
@@ -0,0 +1,9 @@
+import { Image, SafeAreaView, Text, View } from "react-native";
+
+export const ProfileScreen = () => {
+    return(
+        <View>
+            <Text>Profile to user</Text>
+        </View>
+    );
+};
